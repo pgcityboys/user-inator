eec8dc7ad289a51ba4e9f276b0645ead17177564
lukrzak <lukrzak@wp.pl>
2024-01-11 00:22:11 +0100

    Database connection

--- src/test/java/com/pgcityboys/userinator/UserinatorApplicationTests.java
+++ src/test/java/com/pgcityboys/userinator/UserinatorApplicationTests.java
@@ -3,11 +3,7 @@ package com.pgcityboys.userinator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class UserinatorApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
