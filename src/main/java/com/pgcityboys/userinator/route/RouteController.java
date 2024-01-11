250db8a26c7a6c32c6fce9b399ef588c6ccec0e0
lukrzak <lukrzak@wp.pl>
2024-01-11 02:25:46 +0100

    Added CORS policy to allow frontend

--- src/main/java/com/pgcityboys/userinator/route/RouteController.java
+++ src/main/java/com/pgcityboys/userinator/route/RouteController.java
@@ -6,6 +6,7 @@ import com.pgcityboys.userinator.route.dtos.SaveRouteRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/routes")
 @AllArgsConstructor
--- src/main/java/com/pgcityboys/userinator/user/UserController.java
+++ src/main/java/com/pgcityboys/userinator/user/UserController.java
@@ -3,11 +3,13 @@ package com.pgcityboys.userinator.user;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/users")
 @AllArgsConstructor
